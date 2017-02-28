6ab782b0094982e048e696d9aacaba177bfc3e4e
yinyanlv <yanlv.yin@gmail.com>
2017-02-28 14:21:23 +0800

    var => let

--- core/connectDb.js
+++ core/connectDb.js
@@ -2,17 +2,18 @@ let mongoose = require('mongoose');
 let db = null;
 
 function connectDb(connectString) {
+
   db = mongoose.createConnection(connectString);
 
   db.on('open', function (err) {
-    if (err) {
-      console.log(err);
-    }
+
+    if (err) console.log(err);
 
     console.log('mongoose has connected to ' + connectString + '!');
   });
 
   db.on('error', function (err) {
+
     console.log(err);
   });
 
